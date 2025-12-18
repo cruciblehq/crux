5b8fe326f3de14633e3654bcccbe7c30c90f199d
André Francisco <af@neutral.pt>
2025-12-18 21:41:00 +0000

    chore: add root command

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25.1
 require (
 	github.com/BurntSushi/toml v1.6.0
 	github.com/adrg/xdg v0.5.3
+	github.com/alecthomas/kong v1.13.0
 	github.com/fsnotify/fsnotify v1.9.0
 	github.com/go-viper/mapstructure/v2 v2.4.0
 	github.com/klauspost/compress v1.18.2
---
+++ internal/cli/root.go
@@ -0,0 +1,43 @@
+package cli
+
+import (
+	"log/slog"
+
+	"github.com/alecthomas/kong"
+	"github.com/cruciblehq/crux/internal"
+)
+
+var RootCmd struct {
+
+	// Global flags
+	Verbose bool `short:"v" help:"Enable verbose output."`
+	Debug   bool `short:"d" help:"Enable debug output."`
+
+	// Subcommands
+	// Scaffold ScaffoldCmd `cmd:"" aliases:"init,create,new" help:"Scaffold a Crucible resource."`
+	// Build    BuildCmd    `cmd:"" help:"Build and bundle Crucible resources."`
+	// Server   ServerCmd   `cmd:"" help:"Manage the local development server."`
+
+	// Version flag
+	Version kong.VersionFlag `short:"V" help:"Show version information."`
+}
+
+// Parses arguments and runs the CLI
+func Execute() error {
+
+	ctx := kong.Parse(&RootCmd,
+		kong.Name("crux"),
+		kong.Description("Crucible's build tool.\n\nEnables scaffolding, testing, building, packaging, and distributing of Crucible resources."),
+		kong.UsageOnError(),
+		kong.Vars{
+			"version": internal.VersionString(),
+		},
+	)
+
+	if err := ctx.Run(); err != nil {
+		slog.Error("could not parse CLI arguments", "error", err)
+		return err
+	}
+
+	return nil
+}
