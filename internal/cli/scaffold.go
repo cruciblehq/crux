8c80286da4558df12d5fd644730e697534165e95
André Francisco <af@neutral.pt>
2025-12-18 21:41:32 +0000

    chore: add scaffold command

---
+++ internal/cli/scaffold.go
@@ -0,0 +1,13 @@
+package cli
+
+import "github.com/cruciblehq/crux/pkg/crex"
+
+// ScaffoldCmd scaffolds a new Crucible resource.
+type ScaffoldCmd struct {
+	Reference string `arg:"" optional:"" help:"Reference in the template context."`
+}
+
+func (c *ScaffoldCmd) Run() error {
+	return crex.ProgrammingError("cannot initialize new resource project", "scaffold command not implemented yet").
+		Err()
+}
